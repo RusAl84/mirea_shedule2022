499dbd153b74b4beddd5432fdef7e00e32d7bd7f
Aleksey <rusal@bk.ru>
2022-08-31 15:47:12 +0300

    fix urls

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ if __name__ == '__main__':
              "ikts-1K-.xls"],
             ["https://webservices.mirea.ru/upload/iblock/7d1/rfruyo6rz2sljq9qz9k2fcco248jaw8s/IKTST-2k.xls",
              "IKTST-2k.xls"],
-            [
-                "https://webservices.mirea.ru/upload/iblock/1e9/5q85fces7rll3lb131dwv03ln5kqjr7v/IKTST-3-k.xls",
-                "IKTST-3-k.xls"],
+            ["https://webservices.mirea.ru/upload/iblock/96f/eajusa4h33wuu4q2oattflmjmk3am63u/IKTST_3_k_osen-22_23.xls",
+                "IKTST_3_k_osen-22_23.xls"],
             ["https://webservices.mirea.ru/upload/iblock/b8f/qvb2m42wqi7zu4h6cdg7k3m7uveip7o5/IKTST-4-k.xls",
              "IKTST-4-k.xls"],
             ["https://webservices.mirea.ru/upload/iblock/14c/kkk53zx3zxk37b9ivuowt2qgalkkrr8r/IKTST-5-k.xls",
