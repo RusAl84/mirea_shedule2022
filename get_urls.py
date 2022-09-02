01783f6662f91861449893c6bfff9607a9138917
Aleksey <rusal@bk.ru>
2022-09-02 11:01:19 +0300

    auto load urls

--- get_urls.py
+++ get_urls.py
@@ -1,39 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
-url = 'https://www.mirea.ru/schedule/'
-page = requests.get(url)
-#Проверим подключение:
-if page.status_code==200:
-    print(url + " 200")
 
-#Самое время воспользоваться BeautifulSoup4 и скормить ему наш page,
-#указав в кавычках как он нам поможет 'html.parcer':
-soup = BeautifulSoup(page.text, "html.parser")
-#Если попросить его показать, что он там сохранил:
-#print(soup)
+def get_urls(url = 'https://www.mirea.ru/schedule/'):
+    page = requests.get(url)
+    # if page.status_code == 200:
+    #     print(url + " 200")
 
-# #Теперь воспользуемся функцией поиска в BeautifulSoup4:
-# news = soup.findAll('a', class_='lenta')
-insts=[]
-# print(soup)
-blocks = soup.findAll(True, {"class":["uk-width-1-1", "uk-grid-margin", "uk-first-column"]})
-# blocks = soup.findAll('div', class_='uk-width-1-1 uk-grid-margin uk-first-column')
-for block in blocks:
-    print(block)
+    soup = BeautifulSoup(page.text, "html.parser")
+    insts = []
 
+    blocks = soup.findAll(True, {"class": ["uk-card", "slider_ads", "uk-card-body", "uk-card-small"]})
 
-insts = soup.findAll('a', class_='uk-text-bold')
+    urls = []
+    for block in blocks:
+        soup_inst = BeautifulSoup(str(block), "html.parser")
+        inst = soup_inst.find_all("a", {"class": "uk-text-bold"})
+        if len(inst) > 0:
+            # print(inst[0].text)  # список институтов
+            if inst[0].text == 'Институт кибербезопасности и цифровых технологий':
+                # print(inst[0].text)
+                # print(block)
+                num = 1
+                for link in soup_inst.find_all('a', href=True):
+                    # print(link['href'])
+                    if "javascript:void(0)" not in link['href']:
+                        # print(link['href'])
+                        url = []
+                        url.append(link['href'])
+                        url.append(str(num) + "-k.xls")
+                        urls.append(url)
+    return urls
 
-# insts = soup.findAll('a.uk-text-bold')
-# print(insts)
-# for inst in insts:
-#     inst_name = inst.renderContents().decode("utf-8")
-#     # print(inst_name)
-#     if inst_name == "Институт кибербезопасности и цифровых технологий":
-#         print("fd")
-#         print(inst)
-# for news_item in news:
-#     if news_item.find('span', class_='time2 time3') is not None:
-#         new_news.append(news_item.text)
-#
-# print(f"{news_item =}")
+if __name__ == '__main__':
+    get_urls()
--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import get_shedule_files
 import parsing
+import get_urls
 
 if __name__ == '__main__':
-    # print_hi('PyCharm')
-    # for group_col_num in range(0, 6, 5):
-    #     print(group_col_num)
-    urls = [["https://webservices.mirea.ru/upload/iblock/173/ahnk7937c2lon8u8m48s1a97uaqye7vm/1-kurs-IKB-pyatnitsa-nechet.xls",
-             "1K.xls"],
-            ["https://webservices.mirea.ru/upload/iblock/499/9uhqf0wm1na1v020a0gn5zar6856b7vf/2_kurs_IKB_pyatnitsa_nechet.xls",
-             "2k.xls"],
-            ["https://webservices.mirea.ru/upload/iblock/7a3/5mj5kxlbmtx5tehfk4gk9c9muwk220db/3-kurs-IKB-pyatnitsa-nechet.xls",
-            "3k.xls"],
-            ["https://webservices.mirea.ru/upload/iblock/a44/23et1pvdcwn4zonu57qudt63m9tbcqjf/4_kurs_IKB_pyatnitsa_nechet.xls",
-             "4k.xls"],
-            ["https://webservices.mirea.ru/upload/iblock/553/8941gvzxzh0wnoyijohdv3f5nyka9izn/5-kurs-IKB-pyatnitsa-nechet.xls",
-             "5k.xls"]
-            ]
+
+    # urls = [["https://webservices.mirea.ru/upload/iblock/173/ahnk7937c2lon8u8m48s1a97uaqye7vm/1-kurs-IKB-pyatnitsa-nechet.xls",
+    #          "1K.xls"],
+    #         ["https://webservices.mirea.ru/upload/iblock/499/9uhqf0wm1na1v020a0gn5zar6856b7vf/2_kurs_IKB_pyatnitsa_nechet.xls",
+    #          "2k.xls"],
+    #         ["https://webservices.mirea.ru/upload/iblock/7a3/5mj5kxlbmtx5tehfk4gk9c9muwk220db/3-kurs-IKB-pyatnitsa-nechet.xls",
+    #         "3k.xls"],
+    #         ["https://webservices.mirea.ru/upload/iblock/a44/23et1pvdcwn4zonu57qudt63m9tbcqjf/4_kurs_IKB_pyatnitsa_nechet.xls",
+    #          "4k.xls"],
+    #         ["https://webservices.mirea.ru/upload/iblock/553/8941gvzxzh0wnoyijohdv3f5nyka9izn/5-kurs-IKB-pyatnitsa-nechet.xls",
+    #          "5k.xls"]
+    #         ]
+    urls = get_urls.get_urls()
     get_shedule_files.load_all_files(urls)
     filenames = []
     for item_urls in urls:
