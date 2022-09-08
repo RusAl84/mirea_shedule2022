690109ea9fbb4b9710eaa4ab15e8b52601dc0fd2
Aleksey <rusal@bk.ru>
2022-09-08 22:04:59 +0300

    empty auds

--- get_empty_aud.py
+++ get_empty_aud.py
@@ -2,7 +2,10 @@ import urllib
 import pandas as pd
 
 
-def get_all_aud(subj_records):
+def get_all_aud():
+    df = pd.read_excel("output.xlsx")
+    auds = df["aud_name"]
+    all_auds = get_all_auds(auds)
     dweek = {}
     dweek[0] = "не четная"
     dweek[1] = "четная"
@@ -13,10 +16,32 @@ def get_all_aud(subj_records):
     dnum_day[3] = "4ЧТ"
     dnum_day[4] = "5ПТ"
     dnum_day[5] = "6СБ"
+    lines = []
     for num_day in range(6):
         for num_subj in range(1, 7):
             for week in range(0, 2):
-                s = 1
+                line = []
+                line.append(dnum_day[num_day])
+                line.append(num_subj)
+                line.append(dweek[week])
+                empty_auds = get_empty_auds(df, all_auds, num_day, num_subj, week)
+                empty_auds_komp=[]
+                for item in empty_auds:
+                    if "комп" in item:
+                        empty_auds_komp.append(item)
+                str1 = ""
+                for item in empty_auds:
+                    str1 += " " + item
+                line.append(str1)
+                str1 = ""
+                for item in empty_auds_komp:
+                    str1 += " " + item
+                line.append(str1)
+                lines.append(line)
+    list_of_colums = ["num_day", "num_subj", "week", "empty_auds", "empty_auds_komp"]
+    df = pd.DataFrame(lines, columns=list_of_colums)
+    # print(df)
+    df.to_excel("empty_aud.xlsx")
 
 
 def get_empty_auds(df, all_auds, num_day, num_subj, week):
@@ -32,23 +57,27 @@ def get_empty_auds(df, all_auds, num_day, num_subj, week):
     dnum_day[5] = "6СБ"
     list_of_colums = ["inst", "group", "num_day", "num_subj", "week", "subj_name", "subj_type", "teach_name",
                       "aud_name"]
-    # titanic[titanic["Age"] > 35]
-    df_num_day = df[df['num_day'] == num_day]
-    # df_num_day.to_excel("empty_aud.xlsx")
+    df_num_day = df[df['num_day'] == dnum_day[num_day]]
+    # df_num_day.to_excel("df_num_day.xlsx")
     # print(list(df_num_day.columns))
-    df_num_subj = df_num_day[df_num_day['num_subj'] == str(num_subj)]
+    df_num_subj = df_num_day[df_num_day['num_subj'] == num_subj]
     # print(df_num_subj)
-    # df_num_subj.to_excel("empty_aud.xlsx")
+    # df_num_subj.to_excel("df_num_subj.xlsx")
     df_week = df_num_subj[df_num_subj['week'] == dweek[week]]
     c_auds = df_week["aud_name"].values.tolist()
     empty_auds = []
     for aud in c_auds:
         if aud in all_auds:
             empty_auds.append(aud)
+    s = set(empty_auds)
+    empty_auds = list(s)
+    empty_auds.sort()
     return empty_auds
 
 
 def get_all_auds(auds):
+    # print(all_auds)
+    # print(empty_auds)
     all_auds = set(auds)
     l_all_auds = set()
     for item in all_auds:
@@ -82,19 +111,4 @@ def get_all_auds(auds):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    df = pd.read_excel("output.xlsx")
-    auds = df["aud_name"]
-    all_auds = get_all_auds(auds)
-    dweek = {}
-    dweek[0] = "не четная"
-    dweek[1] = "четная"
-    dnum_day = {}
-    dnum_day[0] = "1ПН"
-    dnum_day[1] = "2ВТ"
-    dnum_day[2] = "3СР"
-    dnum_day[3] = "4ЧТ"
-    dnum_day[4] = "5ПТ"
-    dnum_day[5] = "6СБ"
-    empty_auds = get_empty_auds(df, all_auds, 3, 1, 1)
-    print(all_auds)
-    print(empty_auds)
+    get_all_aud()
